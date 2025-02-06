ac1e85d76bc6bf50d2a05688fe76a3ef31afe86e
huu trinh <huutrinh1310@gmail.com>
2025-02-06 22:53:28 +0700

    add return home when error not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { Route, Routes, useLocation } from "react-router";
+import { Link, Route, Routes, useLocation } from "react-router";
 import { RootLayout } from "@/layout/RootLayout";
 import { QuizMaker } from "./pages/quiz-maker/QuizMaker";
 import { QuizResult } from "./pages/quiz-result/QuizResult";
+import { Button } from "./components/ui/button";
+import { Home } from "lucide-react";
 
 function App() {
   const location = useLocation();
@@ -23,7 +25,17 @@ function App() {
       </Route>
       <Route
         path={"*"}
-        element={<div>Not found!</div>}
+        element={
+          <div className="flex flex-col items-center justify-center h-[calc(100vh-4rem)]">
+            Not found!{" "}
+            <Link to={"/"}>
+              <Button className={"hover:opacity-80"}>
+                <Home size={15} />
+                Back to quiz maker
+              </Button>
+            </Link>
+          </div>
+        }
       />
     </Routes>
   );
